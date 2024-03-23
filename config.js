2481ab54f4506afffeb75ddd272f8f6375b1f79a
uk-zash <ssik938@gmail.com>
2024-03-23 16:35:36 +0530

    updates

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const connect = mongoose.connect("mongodb+srv://doadmin:y64TVe28Ab9157Hv@onlineorbitt-01353bee.mongo.ondigitalocean.com/admin?authSource=admin&tls=true")
+const connect = mongoose.connect(process.env.MONGO_URL)
 
 connect.then(()=>{
     // console.log("Database Connecctedd")
